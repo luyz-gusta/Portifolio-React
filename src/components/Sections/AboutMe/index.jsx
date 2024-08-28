a5793290fb4e163a4762c9634b0cfb764b382e37
Luiz Gustavo <luizgustavo.sp2020@gmail.com>
2024-08-28 11:06:30 -0300

    Estilizando modo light

--- src/components/Sections/AboutMe/index.jsx
+++ src/components/Sections/AboutMe/index.jsx
@@ -1,7 +1,8 @@
 import useContexts from "../../../hooks/useContext"
 import { useInView } from 'react-intersection-observer';
 import styles from "./aboutMe.module.css";
-import imageLuiz from "../../../assets/luizgustavo2.png";
+import imageLuizDark from "../../../assets/luizgustavoDark.png";
+import imageLuizLight from "../../../assets/luizgustavoLight.png";
 import SlideTech from '../../SlideTech/index';
 
 export default function SobreMim() {
@@ -30,7 +31,7 @@ export default function SobreMim() {
         <p className={styles.textAbout}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit
         </p>
-        <img src={imageLuiz} alt="Imagem do desenvolvedor" />
+        <img src={toogleThemeElement(imageLuizLight, imageLuizDark)} alt="Imagem do desenvolvedor" />
       </div>
       <div className={styles.slides}>
         <SlideTech />
