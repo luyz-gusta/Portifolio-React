e00ec8cf5064b5bec05cb41478aed8dbff2da324
luyz-softsy <luiz.silva@softsy.io>
2024-09-02 11:56:10 -0300

    Footer adicionado

--- src/components/Sections/Contact/index.jsx
+++ src/components/Sections/Contact/index.jsx
@@ -22,54 +22,59 @@ export default function Contact() {
     });
 
     return (
-        <section ref={contactRef} className={`${styles.secContact} ${toogleThemeElement(styles.dark, styles.light)}`} id="contact">
-            <h1>Contato</h1>
-            <div className={styles.containerContact}>
-                <div className={styles.boxMain}>
-                    <img src={luizGustavo} alt="Imagem do desenvolvedor" className={styles.imgLuiz}/>
-                    <div className={styles.boxText}>
-                        <h2>Luiz Gustavo</h2>
-                        <ul className={styles.boxMidia}>
-                            <li>
-                                <img src={toogleThemeElement(mailDark, mailLight)} alt="Icone de email" />
-                                <p>luizgustavo.sp2020@gmail.com</p>
-                            </li>
-                            <li>
-                                <img src={toogleThemeElement(phoneDark, phoneLight)} alt="Icone de email" />
-                                <p>{'(11) 99490-0485'}</p>
-                            </li>
-                            <li>
-                                <img src={toogleThemeElement(locationDark, locationLight)} alt="Icone de email" />
-                                <p>Barueri, SP</p>
-                            </li>
-                        </ul>
+        <>
+            <section ref={contactRef} className={`${styles.secContact} ${toogleThemeElement(styles.dark, styles.light)}`} id="contact">
+                <h1>Contato</h1>
+                <div className={styles.containerContact}>
+                    <div className={styles.boxMain}>
+                        <img src={luizGustavo} alt="Imagem do desenvolvedor" className={styles.imgLuiz} />
+                        <div className={styles.boxText}>
+                            <h2>Luiz Gustavo</h2>
+                            <ul className={styles.boxMidia}>
+                                <li>
+                                    <img src={toogleThemeElement(mailDark, mailLight)} alt="Icone de email" />
+                                    <p>luizgustavo.sp2020@gmail.com</p>
+                                </li>
+                                <li>
+                                    <img src={toogleThemeElement(phoneDark, phoneLight)} alt="Icone de email" />
+                                    <p>{'(11) 99490-0485'}</p>
+                                </li>
+                                <li>
+                                    <img src={toogleThemeElement(locationDark, locationLight)} alt="Icone de email" />
+                                    <p>Barueri, SP</p>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                <div className={styles.socialMedia}>
-                    <BtnCV />
-                    <div className={styles.listMedia}>
-                        <a href="https://github.com/luyz-gusta" className={styles.itemMedia}>
-                            <span>
-                                <FaGithub />
-                            </span>
-                            <p>luyz-gusta</p>
-                        </a>
-                        <a href="https://www.linkedin.com/in/luyz-gusta/" className={styles.itemMedia}>
-                            <span>
-                                <FaLinkedinIn />
-                            </span>
-                            <p>Luiz Gustavo Da Silva</p>
-                        </a>
-                        <a href="https://www.instagram.com/luyz.gusta/" className={styles.itemMedia}>
-                            <span>
-                                <FaInstagram />
-                            </span>
-                            <p>@luyz.gusta</p>
-                        </a>
+                    <div className={styles.socialMedia}>
+                        <BtnCV />
+                        <div className={styles.listMedia}>
+                            <a href="https://github.com/luyz-gusta" className={styles.itemMedia}>
+                                <span>
+                                    <FaGithub />
+                                </span>
+                                <p>luyz-gusta</p>
+                            </a>
+                            <a href="https://www.linkedin.com/in/luyz-gusta/" className={styles.itemMedia}>
+                                <span>
+                                    <FaLinkedinIn />
+                                </span>
+                                <p>Luiz Gustavo Da Silva</p>
+                            </a>
+                            <a href="https://www.instagram.com/luyz.gusta/" className={styles.itemMedia}>
+                                <span>
+                                    <FaInstagram />
+                                </span>
+                                <p>@luyz.gusta</p>
+                            </a>
+                        </div>
                     </div>
-                </div>
 
-            </div>
-        </section>
+                </div>
+            </section>
+            <footer className={`${toogleThemeElement(styles.darkFooter, styles.lightFooter)}`}>
+                <p>&copy; 2024 Luiz Gustavo. Todos os direitos reservados.</p>
+            </footer>
+        </>
     )
 }
